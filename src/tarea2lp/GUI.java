6ae6b949d56684bf14fc5b75b83b0a882ea2685f
Ignacio Tolosa <ignacio.tolosa.12@sansano.usm.cl>
2014-05-09 17:52:31 -0400

    Flat style

--- src/tarea2lp/GUI.java
+++ src/tarea2lp/GUI.java
@@ -1,11 +1,17 @@
 package tarea2lp;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.util.Random;
 import java.lang.Math;
 
@@ -13,75 +19,96 @@ import java.lang.Math;
 public class GUI implements ActionListener{
 	JFrame frame = new JFrame();
 	JButton[][] grid;
-	private int colour;
 	private int joker;
 	static JButton firstButton;
 	static int firstX, firstY;
+	Border customBorder;
+	HashMap <String, Color> normalButtons = new HashMap<String, Color>();
+	HashMap <String, Color> bonusButtons = new HashMap<String, Color>();
+	HashMap <String, Color> allButtons = new HashMap<String, Color>();
+
+	//normal button probability
+	static int nbpercent = 96;
 	
-	public GUI(int width, int length){
-		frame.setLayout(new GridLayout(width,length));
-		grid = new JButton[width][length];		
-		for (int y=0;y<length;y++){
-			for (int x=0;x<width;x++){
-				joker = randInt(0,100);
-				if (joker < 96){
-					colour = randInt(0,5);
-					if (colour == 0){
-						grid[x][y] = new JButton("R");
-						grid[x][y].setBackground(Color.RED);
-					}
-					else if (colour == 1){
-						grid[x][y] = new JButton("B");
-						grid[x][y].setBackground(Color.BLUE);
-					}
-					else if (colour == 2){
-						grid[x][y] = new JButton("O");
-						grid[x][y].setBackground(Color.ORANGE);
-					}
-					else if (colour == 3){
-						grid[x][y] = new JButton("G");
-						grid[x][y].setBackground(Color.GREEN);
-					}
-					else{
-						grid[x][y] = new JButton("Y");
-						grid[x][y].setBackground(Color.YELLOW);
-					}
-					grid[x][y].addActionListener(this);
-					grid[x][y].setActionCommand("Swap-"+x+"-"+y);
+	public JButton createButton(Object key) {
+		JButton b = new JButton((String) key);
+		b.setBackground(allButtons.get(key));
+		b.setOpaque(true);
+		b.setBorder(customBorder);
+		return b;
+	}
+	
+	public Object randKey(HashMap<String, Color> hm) {
+		Object[] values = hm.keySet().toArray();
+		Object randomKey = values[randInt(0, values.length-1)];
+		return randomKey;
+	}
+	
+	
+	public GUI(int width, int height) {
+		try {
+			//temas de compatibilidad para los botones
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (Exception e) { }
+		
+		//Crea un borde personalizado para los botones
+		//cada numero significa margenes, igual que en css el estilo margin
+		Border baseBorder = new EmptyBorder(10, 10, 10, 10);
+		Border lineBorder = BorderFactory.createLineBorder(Color.BLACK);
+		
+		//con esto se mezclan los estilos para lograr el boton con margen
+		customBorder = new CompoundBorder(lineBorder, baseBorder);
+		
+		//aqui se agrega cada boton a un hashmap
+		//con esto se simplifican las llamadas a los botones
+		//y se asocia cada label del boton a su color
+		normalButtons.put("R", Color.RED);
+		normalButtons.put("B", Color.BLUE);
+		normalButtons.put("O", Color.ORANGE);
+		normalButtons.put("G", Color.GREEN);
+		normalButtons.put("Y", Color.YELLOW);
+		bonusButtons.put("$", Color.MAGENTA);
+		bonusButtons.put("&", Color.CYAN);
+		allButtons.putAll(normalButtons);
+		allButtons.putAll(bonusButtons);
+		
+		frame.setLayout(new GridLayout(width, height));
+		grid = new JButton[width][height];
+		JButton tmpbutton;
+		
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				joker = randInt(0, 100);
+				if (joker < nbpercent) {
+					//muestra y registra un boton normal en el listener de eventos
+					tmpbutton = createButton(randKey(normalButtons));
+				} else {
+					//los botones bonus no deberian tener action listener?
+					tmpbutton = createButton(randKey(bonusButtons));
 				}
-				else{
-					colour = randInt(0,1);
-					if (colour == 0){
-						grid[x][y] = new JButton("$");
-						grid[x][y].setBackground(Color.MAGENTA);
-					}
-					else{
-						grid[x][y] = new JButton("&");
-						grid[x][y].setBackground(Color.CYAN);
-					}
-				}
-					
-				frame.add(grid[x][y]);
+				tmpbutton.addActionListener(this);
+				tmpbutton.setActionCommand("Swap-"+x+"-"+y);
+				frame.add(tmpbutton);
+				grid[x][y] = tmpbutton;
 			}
 		}
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
 	}
 	
 	public void twoButtonAction(JButton j, int x, int y){
-		if (firstButton == null){
+		if (firstButton == null) {
 			firstButton = j;
 			firstX = x;
 			firstY = y;
-		}
-		else{
-			if (firstButton == j){
+		} else {
+			if (firstButton == j) {
 				firstButton = null;
 				firstX = 0;
 				firstY = 0;
-			}
-			else{
+			} else {
 				if ( ((Math.abs(firstX-x) == 1) && (firstY == y)) || ((firstX == x) && (Math.abs(firstY-y) == 1)) ){
 					swapButton(firstX,firstY,x,y);
 				}
@@ -95,52 +122,22 @@ public class GUI implements ActionListener{
 	public void swapButton(int x1, int y1, int x2, int y2){
 		JButton j1 = grid[x1][y1];
 		JButton j2 = grid[x2][y2];
-		
-		grid[x1][y1] = new JButton(j2.getText());
-		if (j2.getText().equals("R")){
-			grid[x1][y1].setBackground(Color.RED);
-		}
-		else if(j2.getText().equals("B")){
-			grid[x1][y1].setBackground(Color.BLUE);
-		}
-		else if(j2.getText().equals("O")){
-			grid[x1][y1].setBackground(Color.ORANGE);
-		}
-		else if(j2.getText().equals("G")){
-			grid[x1][y1].setBackground(Color.GREEN);
-		}
-		else{
-			grid[x1][y1].setBackground(Color.YELLOW);
-		}
+		grid[x1][y1] = createButton(j2.getText());
 		grid[x1][y1].addActionListener(this);
 		grid[x1][y1].setActionCommand("Swap-"+x1+"-"+y1);
-		
-		grid[x2][y2] = new JButton(j1.getText());
-		if (j1.getText().equals("R")){
-			grid[x2][y2].setBackground(Color.RED);
-		}
-		else if(j1.getText().equals("B")){
-			grid[x2][y2].setBackground(Color.BLUE);
-		}
-		else if(j1.getText().equals("O")){
-			grid[x2][y2].setBackground(Color.ORANGE);
-		}
-		else if(j1.getText().equals("G")){
-			grid[x2][y2].setBackground(Color.GREEN);
-		}
-		else{
-			grid[x2][y2].setBackground(Color.YELLOW);
-		}
+		grid[x2][y2] = createButton(j1.getText());
 		grid[x2][y2].addActionListener(this);
 		grid[x2][y2].setActionCommand("Swap-"+x2+"-"+y2);
-			
 		frame.remove(j1);
 		frame.remove(j2);
-		for (int y=0;y<15;y++){
-			for (int x=0;x<15;x++){
+		
+		
+		for (int y = 0; y < 15; y++) {
+			for (int x = 0; x < 15; x++) {
 				frame.add(grid[x][y]);
 			}
 		}
+
 		frame.pack();
 		frame.setVisible(true);
 	}
--- src/tarea2lp/Main.java
+++ src/tarea2lp/Main.java
@@ -3,7 +3,7 @@ package tarea2lp;
 public class Main {
 
 	public static void main(String[] args) {
-		new GUI(15,15);
+		new GUI(15, 15);
 	}
 
 }
