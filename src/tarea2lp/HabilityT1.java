b9acab5f81bed56ef75c304380060b9f1ba3e681
Daniel Alejandro Morales Miguel <daniel@devwith.us>
2014-05-10 22:08:33 -0400

    Post merge del apocalipsis

--- src/tarea2lp/HabilityT1.java
+++ src/tarea2lp/HabilityT1.java
@@ -1,8 +1,46 @@
 package tarea2lp;
 
 public class HabilityT1 implements HabilityBehavior {
+	/* public Bloque = block;
+	 * public HabilityT1(Bloque b){
+	 *   this.block = b;
+	 * }
+	 */
+	
 	@Override
 	public void Habilidad(){
 		//Borra toda la fila
+		/*
+		 * for (int x=0;x<15;x++){
+		*       //R,B,O,G,Y
+		*       String colour = grid[block.y][x]; 
+				if (colour.equals("R")){
+					estadisticas[0] += 1;
+				}
+				else if (colour.equals("B")){
+					estadisticas[1] += 1;
+				}
+				else if (colour.equals("O")){
+					estadisticas[2] += 1;
+				}
+				else if (colour.equals("G")){
+					estadisticas[3] += 1;
+				}
+				else if (colour.equals("Y")){
+					estadisticas[4] += 1;
+				}
+		 *     grid[block.y][x].setBackground(Color.WHITE);
+			   grid[block.y][x].setText("-");
+			   for (int r=block.y;r>0;r--){
+			       if(grid[r-1][i].getText().equals("-"))
+				       break;
+					   swapWhite(i,r,i,r-1);
+			   }
+			   
+		 * }
+		 * fillWhites();
+		 * GUI.bloquesFaltantes -= 15;
+		 * setEstadisticasLabel();
+		 */
 	}
 }
--- src/tarea2lp/HabilityT2.java
+++ src/tarea2lp/HabilityT2.java
@@ -4,5 +4,36 @@ public class HabilityT2 implements HabilityBehavior {
 	@Override
 	public void Habilidad(){
 		// Borra toda la columna
+		/* for (int y=0;y<15;y++){
+		        //R,B,O,G,Y
+		        String colour = grid[y][block.x]; 
+				if (colour.equals("R")){
+					estadisticas[0] += 1;
+				}
+				else if (colour.equals("B")){
+					estadisticas[1] += 1;
+				}
+				else if (colour.equals("O")){
+					estadisticas[2] += 1;
+				}
+				else if (colour.equals("G")){
+					estadisticas[3] += 1;
+				}
+				else if (colour.equals("Y")){
+					estadisticas[4] += 1;
+				}
+		       grid[y][block.x].setBackground(Color.WHITE);
+			   grid[y][block.x].setText("-");
+			   for (int r=block.y;r>0;r--){
+			       if(grid[r-1][i].getText().equals("-"))
+				       break;
+					   swapWhite(i,r,i,r-1);
+			   }
+			   
+		 * }
+		 * fillWhites();
+		 * GUI.bloquesFaltantes -= 15;
+		 * setEstadisticasLabel();
+		 */
 	}
 }
