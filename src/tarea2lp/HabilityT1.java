94b900ba8ff277f7b05b6724a2b842a50c4286f0
Daniel Alejandro Morales Miguel <daniel@devwith.us>
2014-05-11 18:19:48 -0400

    Habilidades creadas

--- src/tarea2lp/HabilityT1.java
+++ src/tarea2lp/HabilityT1.java
@@ -1,46 +1,45 @@
 package tarea2lp;
+import java.awt.Color;
 
 public class HabilityT1 implements HabilityBehavior {
-	/* public Bloque = block;
-	 * public HabilityT1(Bloque b){
-	 *   this.block = b;
-	 * }
-	 */
+	public Bloque block;
 	
+	public HabilityT1(Bloque b){
+		this.block = b;
+	}
+		
 	@Override
 	public void Habilidad(){
 		//Borra toda la fila
-		/*
-		 * for (int x=0;x<15;x++){
-		*       //R,B,O,G,Y
-		*       String colour = grid[block.y][x]; 
-				if (colour.equals("R")){
-					estadisticas[0] += 1;
-				}
-				else if (colour.equals("B")){
-					estadisticas[1] += 1;
-				}
-				else if (colour.equals("O")){
-					estadisticas[2] += 1;
-				}
-				else if (colour.equals("G")){
-					estadisticas[3] += 1;
-				}
-				else if (colour.equals("Y")){
-					estadisticas[4] += 1;
-				}
-		 *     grid[block.y][x].setBackground(Color.WHITE);
-			   grid[block.y][x].setText("-");
-			   for (int r=block.y;r>0;r--){
-			       if(grid[r-1][i].getText().equals("-"))
-				       break;
-					   swapWhite(i,r,i,r-1);
-			   }
+		for (int x=0;x<15;x++){
+			//R,B,O,G,Y
+			String colour = GUI.grid[block.y][x].getText(); 
+			if (colour.equals("R")){
+				GUI.estadisticas[0] += 1;
+			}
+			else if (colour.equals("B")){
+				GUI.estadisticas[1] += 1;
+			}
+			else if (colour.equals("O")){
+				GUI.estadisticas[2] += 1;
+			}
+			else if (colour.equals("G")){
+				GUI.estadisticas[3] += 1;
+			}
+			else if (colour.equals("Y")){
+				GUI.estadisticas[4] += 1;
+			}
+			GUI.grid[block.y][x].setBackground(Color.WHITE);
+			GUI.grid[block.y][x].setText("-");
+			for (int r=block.y;r>0;r--){
+				if(GUI.grid[r-1][x].getText().equals("-"))
+					break;
+				   	GUI.swapWhite(x,r,x,r-1);
+			}
 			   
-		 * }
-		 * fillWhites();
-		 * GUI.bloquesFaltantes -= 15;
-		 * setEstadisticasLabel();
-		 */
+		}
+		GUI.fillWhites();
+		GUI.bloquesFaltantes -= 15;
+		GUI.setEstadisticasLabel();
 	}
 }
--- src/tarea2lp/HabilityT2.java
+++ src/tarea2lp/HabilityT2.java
@@ -1,39 +1,46 @@
 package tarea2lp;
 
+import java.awt.Color;
+
 public class HabilityT2 implements HabilityBehavior {
+	public Bloque block;
+	
+	public HabilityT2(Bloque b){
+		this.block = b;
+	}
+	
 	@Override
 	public void Habilidad(){
-		// Borra toda la columna
-		/* for (int y=0;y<15;y++){
-		        //R,B,O,G,Y
-		        String colour = grid[y][block.x]; 
-				if (colour.equals("R")){
-					estadisticas[0] += 1;
-				}
-				else if (colour.equals("B")){
-					estadisticas[1] += 1;
-				}
-				else if (colour.equals("O")){
-					estadisticas[2] += 1;
-				}
-				else if (colour.equals("G")){
-					estadisticas[3] += 1;
-				}
-				else if (colour.equals("Y")){
-					estadisticas[4] += 1;
-				}
-		       grid[y][block.x].setBackground(Color.WHITE);
-			   grid[y][block.x].setText("-");
-			   for (int r=block.y;r>0;r--){
-			       if(grid[r-1][i].getText().equals("-"))
-				       break;
-					   swapWhite(i,r,i,r-1);
-			   }
+		//Borra toda la columna
+		for (int y=0;y<15;y++){
+			//R,B,O,G,Y
+			String colour = GUI.grid[y][block.x].getText(); 
+			if (colour.equals("R")){
+				GUI.estadisticas[0] += 1;
+			}
+			else if (colour.equals("B")){
+				GUI.estadisticas[1] += 1;
+			}
+			else if (colour.equals("O")){
+				GUI.estadisticas[2] += 1;
+			}
+			else if (colour.equals("G")){
+				GUI.estadisticas[3] += 1;
+			}
+			else if (colour.equals("Y")){
+				GUI.estadisticas[4] += 1;
+			}
+	       GUI.grid[y][block.x].setBackground(Color.WHITE);
+		   GUI.grid[y][block.x].setText("-");
+		   for (int r=block.y;r>0;r--){
+		       if(GUI.grid[r-1][block.x].getText().equals("-"))
+			       break;
+				   GUI.swapWhite(block.x,r,block.x,r-1);
+		   }
 			   
-		 * }
-		 * fillWhites();
-		 * GUI.bloquesFaltantes -= 15;
-		 * setEstadisticasLabel();
-		 */
+		 }
+		 GUI.fillWhites();
+		 GUI.bloquesFaltantes -= 15;
+		 GUI.setEstadisticasLabel();
 	}
 }
